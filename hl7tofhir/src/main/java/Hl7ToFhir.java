778aca1a722b4a3906a0b051949e7ac7d5d3b585
tspegel <spegel.tjaz15@gmail.com>
2019-04-22 19:06:49 +0200

    First commit

---
+++ hl7tofhir/src/main/java/Hl7ToFhir.java
@@ -0,0 +1,15 @@
+import lombok.Getter;
+import lombok.Setter;
+import org.hl7.fhir.r4.model.Patient;
+
+public class Hl7ToFhir {
+
+    /**
+     * Transforme PID part of hl7v2 message belowe to FHIR Patient (R4) resource.
+     */
+
+    private Patient mapHl7toFhir(){
+        return null;
+    }
+
+}
---
+++ hl7tofhir/src/test/java/Hl7ToFhirTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Hl7ToFhirTest {
+
+    private Hl7ToFhir hl7ToFhir;
+
+    @Before
+    public void setUp(){
+        hl7ToFhir = new Hl7ToFhir();
+    }
+
+    @Test
+    public void stuff() {
+
+    }
+}
